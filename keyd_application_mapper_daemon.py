a57477b09018b03976a95a958616fb11d0d33233
Luis De Avila <bizangel10@gmail.com>
2025-11-27 00:54:27 +0000

    Working

--- keyd_application_mapper_daemon.py
+++ keyd_application_mapper_daemon.py
@@ -1,12 +1,59 @@
 #!/usr/bin/python3
-import socket, os, subprocess, grp
+import socket, os, grp, sys
+from fnmatch import fnmatch
+import subprocess
 
 SOCKET_PATH = "/run/keyd_application_switcher_daemon.sock"
 ACCESS_GROUP = "keyd-application-switcher"
+CONFIG_PATH = "/etc/keyd_application_switcher/app.conf"
+KEYD_BIN = os.environ.get('KEYD_BIN', 'keyd')
+debug_flag = os.getenv('KEYD_DEBUG')
+
+def dbg(s):
+    if debug_flag:
+        print(s)
+
+def die(msg):
+    sys.stderr.write('ERROR: ')
+    sys.stderr.write(msg)
+    sys.stderr.write('\n')
+    exit(0)
+
+def parse_config(path):
+    config = []
+
+    for line in open(path):
+        line = line.strip()
+
+        if line.startswith('[') and line.endswith(']'):
+            a = line[1:-1].split('|')
+
+            if len(a) < 2:
+                cls = a[0]
+                title = '*'
+            else:
+                cls = a[0]
+                title = a[1]
+
+            bindings = []
+            config.append((cls, title, bindings))
+        elif line == '':
+            continue
+        elif line.startswith('#'):
+            continue
+        else:
+            bindings.append(line)
+
+    return config
 
 # Ensure socket exists
 if os.path.exists(SOCKET_PATH):
     os.remove(SOCKET_PATH)
+
+if not os.path.exists(CONFIG_PATH):
+    die('could not find app.conf, make sure it is in /etc/keyd_application_switcher/app.conf')
+
+config = parse_config(CONFIG_PATH)
 
 server = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 server.bind(SOCKET_PATH)
@@ -14,12 +61,40 @@ os.chown(SOCKET_PATH, 0, grp.getgrnam(ACCESS_GROUP).gr_gid)
 os.chmod(SOCKET_PATH, 0o660)  # perms
 server.listen(1)
 
+dbg("Debug mode on")
 print(f"Keyd Application Switcher Daemon Listening on {SOCKET_PATH}... ")
+
+def lookup_bindings(cls, title):
+    bindings = []
+    for cexp, texp, b in config:
+        if fnmatch(cls, cexp) and fnmatch(title, texp):
+            dbg(f'\tMatched {cexp}|{texp}')
+            bindings.extend(b)
+
+    return bindings
+
+last_mtime = os.path.getmtime(CONFIG_PATH)
+def on_window_change(cls, title):
+    global last_mtime
+    global config
+
+    mtime = os.path.getmtime(CONFIG_PATH)
+
+    if mtime != last_mtime:
+        print(CONFIG_PATH + ': Updated, reloading config...')
+        config = parse_config(CONFIG_PATH)
+        last_mtime = mtime
+
+    bindings = lookup_bindings(cls, title)
+    subprocess.run([KEYD_BIN, 'bind', 'reset', *bindings], stdout=subprocess.DEVNULL)
 
 while True:
     conn, _ = server.accept()
     data = conn.recv(128).decode().strip()
-
-    print("received: ", data)
+    data = [x.strip() for x in data.split('|') if x.strip() != ""]
+    if len(data) == 2:
+        classname, title = data
+        dbg(f"Calling on_window_change hook with {classname}|{title}")
+        on_window_change(classname, title)
 
     conn.close()
--- readme.md
+++ readme.md
@@ -9,12 +9,13 @@ Given the code is quite simple I thought I'd just change it a bit:
 
 1. A daemon server listens on a unix socket specifically `/run/keyd_application_switcher_daemon.sock` this daemon reads the config and triggers the actual keyd changes.
 1.1. The daemon only reads from the untrusted socket input classnames and window titles for the matching logic. This prevents any sort of privilege escalation.
-1.2. It is then very important that the configuration file permissions are protected to only root.
+1.2. It is then very important that the configuration file permissions are protected to only root. (`/etc/keyd_application_switcher/app.conf`)
 
 2. The user client will simply use the previously existing logic to detect whenever a window changes - and send this to the switcher daemon by writing into the unix socket. This will trigger the remapping.
 
 For a bit extra security users must be part of `keyd-application-switcher` group to write to the socket.
 
+**NOTE: Bear in mind that this is based on the python script of keyd - so it is still relatively experimental**
 
 ## Quickstart
 
@@ -30,7 +31,27 @@ sudo groupadd keyd-application-switcher
 sudo usermod -aG keyd-application-switcher "$(whoami)"
 ```
 
-3.
+3. Reboot so that group permissions are re-loaded. (For me logout didn't work)
+
+4. Create the config you wish to use:
+
+```sh
+sudo mkdir -p /etc/keyd_application_switcher/
+sudo touch /etc/keyd_application_switcher/app.conf
+sudo nano /etc/keyd_application_switcher/app.conf
+```
+
+Fill it out with contents as you wish - example:
+
+```
+[org-mozilla-firefox]
+
+q = a
+a = q
+```
+
+5. Set up the services.
+
 
 
 
